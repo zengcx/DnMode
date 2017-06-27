1e1e14b9838eda82f66ca6160c790d0b9e44b70b
dora <zengchaoxing@sina.cn>
2017-06-27 17:58:29 +0800

    test

--- java_agency/src/impl/LogHandler.java
+++ java_agency/src/impl/LogHandler.java
@@ -14,7 +14,7 @@ public class LogHandler implements InvocationHandler {
 		// 第一个参数指定代理对象的类加载器，需要指定为和目标对象同一个类加载器
 		// 第二个参数要实现和目标对象一样的接口，所以只需要拿到目标对象的实现接口
 		// 第三个参数表明这些被拦截的方法在被拦截时需要执行哪个invocationhandler的invoke方法
-		// 根据传入的目标返回一个代理对象
+		// 根据传入的目标返回一个代理对象.
 		return Proxy.newProxyInstance(targetObject.getClass().getClassLoader(), targetObject.getClass().getInterfaces(),
 				this);
 
